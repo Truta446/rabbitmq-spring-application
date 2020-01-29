a45669c8e2873938ed703f4f43aec4f2cb6069ba
Juan Lopes <juan.versolato@outlook.com>
2020-01-29 19:11:38 -0400

    Project using Java, Maven, RabbitMQ and other libs.

---
+++ spring-procuder/src/main/java/com/example/spring/producer/SpringProcuderApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProcuderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProcuderApplication.class, args);
+	}
+
+}
---
+++ spring-procuder/src/test/java/com/example/spring/producer/SpringProcuderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProcuderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
